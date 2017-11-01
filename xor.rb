ee2aec69b3e30514a21efac3abbd6575051328b1
Peter Tseng <pht24@cornell.edu>
2017-11-01 23:22:46 +0000

    crack_single: square the difference
    Still gives same answers, so we good

--- xor.rb
+++ xor.rb
@@ -53,7 +53,8 @@ def crack_single(bytes, must_be_printable: true)
       i,
       ENGLISH_FREQ.zip(?a..?z).map { |expect_freq, letter|
         got_freq = (freqs[letter]&.to_f &./ lower_english_only.size) || 0
-        (expect_freq - got_freq).abs
+        # Inspired by least-squares regressions, take the square
+        (expect_freq - got_freq) ** 2
       }.sum
     ]
   }
