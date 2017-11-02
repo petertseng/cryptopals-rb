e87a1736fa829cbb21cf074d5aa0948090ca92d2
Peter Tseng <pht24@cornell.edu>
2017-11-02 06:11:54 +0000

    Challenge 07: Add final
    Necessary because see documentation:
    http://ruby-doc.org/stdlib-2.0.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-final
    
    Without this, the final few chars of the answer are not shown!

--- 07_aes_ecb.rb
+++ 07_aes_ecb.rb
@@ -4,4 +4,4 @@ require 'openssl'
 bytes = Base64.decode64(File.read('data/07.txt'))
 cipher = OpenSSL::Cipher::AES.new(128, :ECB).decrypt
 cipher.key = 'YELLOW SUBMARINE'
-puts cipher.update(bytes) if ARGV.include?('-v')
+puts cipher.update(bytes) + cipher.final if ARGV.include?('-v')
