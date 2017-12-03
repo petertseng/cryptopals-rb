4a111e1f35641475caa175deb31fd52e17482a83
Peter Tseng <pht24@cornell.edu>
2017-12-03 02:23:44 +0000

    Challenge 28

--- 28_sha1_mac.rb
+++ 28_sha1_mac.rb
@@ -9,3 +9,10 @@ s = ''
   s << ?a
   assert_eq(Digest::SHA1::hexdigest(s), sha1(s), s)
 }
+
+key = Array.new(16) { rand(256) }.pack('c*').freeze
+
+message = 'hello'
+mac = sha1(key + message)
+
+assert_eq(false, mac == sha1(key + 'jello'))
